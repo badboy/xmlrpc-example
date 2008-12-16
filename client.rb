9352013f024909b69932e4726be180012847bb51
BadBoy <psp.badboy@gmail.com>
2008-12-16 21:09:53 +0100

    first push

---
+++ client.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+require "xmlrpc/client"
+
+$client = XMLRPC::Client.new( "localhost", "/", 8080)
+
+def callxml(met, *args)
+  begin
+    puts "#{met}(#{args.join(', ')})"
+    p $client.call(met, *args)
+  rescue Exception => boom
+    puts " !!! exception caught: #{boom}"
+    puts "  "+boom.message
+  end
+  puts
+end
+
+callxml "sample.sumAndDifference", 3,5
+callxml "sample.requestLogin", 'test'
+callxml "sample.requestLogin", 42
+callxml "sample.requestLogin2", 'test'
+callxml "sample.requestLogin2", 42
+callxml "sample.gettoken"
+callxml "sample.zero"
+callxml "sample.l", 7,8
---
+++ server.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+
+require "xmlrpc/server"
+
+s = XMLRPC::Server.new(8080)
+
+$tokens=Hash.new;
+
+s.add_handler("sample.sumAndDifference") do |a,b|
+  { "sum" => a + b, "difference" => a - b }
+end
+
+s.add_handler("sample.requestLogin") do |username|
+    token=rand(36**9).to_s(36)
+    $tokens[username]={:token => token, :time => Time.new}
+    return {"token" => token}
+    { "sum" => 2 }
+end
+
+
+s.add_handler("sample.requestLogin2") do |username|
+    token=rand(36**9).to_s(36)
+    $tokens[username]={:token => token, :time => Time.new}
+    {"token" => token}
+end
+
+s.add_handler("sample.gettoken") do
+  $tokens
+end
+# geht garantiert nicht
+s.add_handler("sample.zero") do
+ asdfasfdasdf;
+ (1/0).to_s;
+end
+
+
+s.add_handler("sample.l") do |a,b|
+  { "sum" => a + b, "difference" => a - b }
+end
+
+
+s.serve
